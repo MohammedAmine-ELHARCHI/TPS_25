1e76aa34b0c80e3a762b2c40c5d5f307e7932d49
MohammedAmine-ELHARCHI <mohammedamineelharchi@gmail.com>
2025-12-30 18:19:04 +0100

    tps

---
+++ TP 14 Client Soap Android Kotlin avec KSOAP/soap-service/src/main/java/ma/projet/soapservice/entity/TypeCompte.java
@@ -0,0 +1,27 @@
+package ma.projet.soapservice.entity;
+
+/**
+ * Enumeration representing the different types of bank accounts.
+ * 
+ * <p>Supported account types:</p>
+ * <ul>
+ *   <li><b>COURANT</b>: Checking account (Compte Courant)</li>
+ *   <li><b>EPARGNE</b>: Savings account (Compte Épargne)</li>
+ * </ul>
+ * 
+ * @author MOHAMMED AMINE ELHARCHI
+ * @version 1.0
+ * @since 2025-01-01
+ */
+public enum TypeCompte {
+    
+    /**
+     * Checking account - used for everyday transactions.
+     */
+    COURANT,
+    
+    /**
+     * Savings account - used for saving money with potential interest.
+     */
+    EPARGNE
+}
